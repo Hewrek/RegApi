26d488e1c684a6d2f0728bc616253202757addd7
Nefi Apablaza <163865515+Hewrek@users.noreply.github.com>
2024-12-19 00:53:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3306;
+const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
